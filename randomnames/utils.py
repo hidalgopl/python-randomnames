4fa3c5d31f6ae48ff88989f2584165f0262a313c
Wiggins <wiggins@concentricsky.com>
2016-03-15 15:03:24 -0700

    polite adjectives and animals

--- randomnames/utils.py
+++ randomnames/utils.py
@@ -6,9 +6,18 @@ import random
 def random_noun():
     return random.choice(NOUNS).title()
 
+def random_animal():
+    return random.choice(ANIMALS).title()
+
 def random_adjective():
     return random.choice(ADJECTIVES).title()
+
+def random_polite_adjective():
+    return random.choice(POLITE_ADJECTIVES).title()
 
 def random_namepair():
     return u"{} {}".format(random_adjective(), random_noun())
 
+
+def random_polite_animalpair():
+    return u"{} {}".format(random_polite_adjective(), random_animal())
